af15bf6a632f0001ff34e58ad4e6442d5fd3dbee
adacola <adacola255@gmail.com>
2026-02-24 21:03:27 +0900

    tmuxの設定を追加

--- install.sh
+++ install.sh
@@ -18,3 +18,7 @@ cp ~/dotfiles/claude-file-suggestion.sh ~/.claude/
 echo "Claude Codeのファイル名補完スクリプト実行に必要なコマンドをインストール"
 sudo apt update && sudo apt install fd-find fzf
 [ ! -e ~/.local/bin/fd ] && ln -s $(which fdfind) ~/.local/bin/fd
+
+echo "tmux をインストール"
+sudo apt install tmux
+[! -e ~/.config/tmux/tmux.conf ] && mkdir -p ~/.config/tmux && cp ~/dotfiles/tmux.conf ~/.config/tmux/
